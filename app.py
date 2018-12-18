86dfb4a842885fffd1bab9c328e1b1894104feed
Qiang Wang <qiang.wang@nutanix.com>
2018-12-18 12:59:30 -0800

    v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "<h1>Billing v1</h1>"
+    return "<h1>Billing v2</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
